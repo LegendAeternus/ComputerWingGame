240e85e5c06f195a0b115d9ef77bc771d11fb11b
cbraun3827 <cbraun1@gmu.edu>
2014-12-13 02:10:48 -0500

    New Classes
    Missile,
    Pilot,
    TieFighter

---
+++ src/xwing/TieFighter.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xwing;
+
+/**
+ *
+ * @author Chris
+ */
+public class TieFighter extends NormalShip {
+    public TieFighter(double Xmm, double Ymm, double orientDeg, int team) {
+        super(Xmm, Ymm, orientDeg, team);
+    }
+}
